5313d16f7cbb9c9406e7cea6a9f3a6c6a7d3fc5b
mustafa-toptal <mustafa.ali@toptal.com>
2022-03-30 22:27:32 +0530

    remove useless code

--- server/server.js
+++ server/server.js
@@ -12,22 +12,22 @@ const io = require("socket.io")(server, {
   },
 });
 
-const apiTimeout = 60 * 60 * 1000;
-app.use((req, res, next) => {
-  // Set the timeout for all HTTP requests
-  req.setTimeout(apiTimeout, () => {
-    let err = new Error("Request Timeout");
-    err.status = 408;
-    next(err);
-  });
-  // Set the server response timeout for all HTTP requests
-  res.setTimeout(apiTimeout, () => {
-    let err = new Error("Service Unavailable");
-    err.status = 508;
-    next(err);
-  });
-  next();
-});
+// const apiTimeout = 60 * 60 * 1000;
+// app.use((req, res, next) => {
+//   // Set the timeout for all HTTP requests
+//   req.setTimeout(apiTimeout, () => {
+//     let err = new Error("Request Timeout");
+//     err.status = 408;
+//     next(err);
+//   });
+//   // Set the server response timeout for all HTTP requests
+//   res.setTimeout(apiTimeout, () => {
+//     let err = new Error("Service Unavailable");
+//     err.status = 508;
+//     next(err);
+//   });
+//   next();
+// });
 
 app.use(bodyParser.json({ limit: "5gb" }));
 
